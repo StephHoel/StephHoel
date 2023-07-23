2744fae3afc0158216b4033b5c76a0af52b063f1
Steph Hoel <steph.hoel@gmail.com>
2023-07-23 14:20:09 -0300

    feat: Add blog

---
+++ blog/src/pages/about_me.tsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+  return (
+    <div>
+      <p className="text-5xl mb-4">Sobre Mim</p>
+      <p className="">
+        Atualmente eu sou DevJr (ou Sr.Assoc) na Avanade Brasil, na área de
+        Engenharia de Software, e graduada em Sistemas para Internet.
+      </p>
+      <p>
+        Estou desenvolvendo alguns projetos no meu pouco tempo livre e todos
+        você pode ver aqui no GitHub. Quem quiser participar de algum, me chame
+        no e-mail ou no discord para vermos onde podemos nos ajudar mutuamente!
+      </p>
+      <p>
+        E não menos importante: meus pronomes são Ela/Ele/Dela/Dele. Fique a
+        vontade para me chamar da forma que quiser!
+      </p>
+      <p>
+        Fun fact: Estudo há mais de 10 anos desenvolvimento web sozinha, fazendo
+        pequenos sites que nunca foram ao ar e que se perderam ao longo dos
+        anos. Até cheguei a desenvolver um RPG de texto em forma de site, mas já
+        perdi os arquivos e não sei se conseguiria fazer de novo. Será que eu
+        devo arriscar?
+      </p>
+    </div>
+  )
+}
