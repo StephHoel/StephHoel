cb19e4b7d299f1e76034f35bbcf8e8d2029064ec
Steph Hoel <steph.hoel@gmail.com>
2023-07-26 08:50:48 -0300

    build: Melhorando Componente Links

--- blog/src/components/posts/Links.tsx
+++ blog/src/components/posts/Links.tsx
@@ -1,13 +1,16 @@
 import { ReactNode } from 'react'
 
 interface Props {
-  href: string
+  href?: string
   children: ReactNode
 }
 
 export default function Links({ href, children }: Props) {
   return (
-    <a href={href} className="text-blue-400 hover:text-blue-300 cursor-pointer">
+    <a
+      href={href || children?.toString()}
+      className="text-blue-400 hover:text-blue-300 cursor-pointer"
+    >
       {children}
     </a>
   )
