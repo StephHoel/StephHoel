6e2c57649a2545c3edcb93c99e32e77aa7782b97
Steph Hoel <steph.hoel@gmail.com>
2023-08-06 11:34:30 -0300

    build: Update Post 4

--- src/posts/P20230722.tsx
+++ src/posts/P20230722.tsx
@@ -63,20 +63,28 @@ export default function P20230722() {
 
         <div>
           <p>
-            6. Configure o arquivo <Code>index.tsx</Code> mudando de:
+            6. Configure o arquivo <Code>index.tsx</Code> mudando todo o código
+            para:
           </p>
           <div className="bg-gray-700 text-amber-600 w-fit px-2 py-1 rounded font-bold ml-8">
-            <p>{'<React.StrictMode>'}</p>
-            <p>&nbsp; {'<App />'}</p>
-            <p>{'</React.StrictMode>'}</p>
-          </div>
-          <p className="ml-8">Para:</p>
-          <div className="bg-gray-700 text-amber-600 w-fit px-2 py-1 rounded font-bold ml-8">
-            <p>{'<React.StrictMode>'}</p>
-            <p>&nbsp;{' <BrowserRouter>'}</p>
-            <p>&nbsp;&nbsp;&nbsp;{' <App />'}</p>
-            <p>&nbsp;{' </BrowserRouter>'}</p>
-            <p>{'</React.StrictMode>'}</p>
+            <p>import React from &apos;react&apos;</p>
+            <p>import ReactDOM from &apos;react-dom/client&apos;</p>
+            <p>import App from &apos;./app&apos;</p>
+            <p>import &apos;./index.css&apos;</p>
+            <p>{"import { BrowserRouter } from 'react-router-dom'"}</p>
+            <p></p>
+            <p>
+              const root =
+              ReactDOM.createRoot(document.getElementById(&apos;root&apos;) as
+              HTMLElement)
+            </p>
+            <p>root.render(</p>
+            <p>&nbsp;&nbsp;{'<React.StrictMode>'}</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;{'<BrowserRouter>'}</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{'<App />'}</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;{'</BrowserRouter>'}</p>
+            <p>&nbsp;&nbsp;{'</React.StrictMode>,'}</p>
+            <p>)</p>
           </div>
         </div>
 
@@ -167,9 +175,11 @@ export default function P20230722() {
           Se usar a dependência TailWindCss, você vai precisar adicionar 3
           linhas no começo do arquivo .css:
         </p>
-        <p>@tailwind base;</p>
-        <p>@tailwind components;</p>
-        <p>@tailwind utilities;</p>
+        <Code>
+          <p>@tailwind base;</p>
+          <p>@tailwind components;</p>
+          <p>@tailwind utilities;</p>
+        </Code>
       </div>
     </Toggle>
   )
